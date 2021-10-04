757cb5dc3acede30f8119a9c04add7cad27bc132
Adarsh N B <adarshnb@gmail.com>
2021-10-04 16:47:58 +0530

    add form spree end point

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     description: "Personal page of Adarsh N B",
     locale: "en",
     title: "Adarsh N B",
+    formspreeEndpoint: "https://formspree.io/f/xdoyjgjn",
   },
   plugins: [
     {
